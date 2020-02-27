03f12a0fd15549869834e2e4f120c060eace0c97
Lleno <launiv3rse@gmail.com>
2020-02-27 16:23:36 +0000

    printBoard now returns TilePane

--- src/First/Board.java
+++ src/First/Board.java
@@ -20,16 +20,6 @@ public class Board{
         for(int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 squares[i][j] = new Square();
-                int finalJ = j;
-                int finalI = i;
-                squares[i][j].setOnAction(new EventHandler<ActionEvent>() {
-
-                    @Override
-                    public void handle(ActionEvent event) {
-                        squares[finalI][finalJ].setPiece(GameLogic.changeTurn());
-                        System.out.println(checkWin());
-                    }
-                });
             }
         }
     }
