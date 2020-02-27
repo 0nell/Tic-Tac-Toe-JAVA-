8501170be19421c7bc6f9c54729dc11d60162257
Lleno <launiv3rse@gmail.com>
2020-02-27 16:21:43 +0000

    Added turn variable

--- src/First/GameLogic.java
+++ src/First/GameLogic.java
@@ -1,42 +1,48 @@
 package First;
+import javafx.scene.layout.TilePane;
+
 import java.util.Scanner;
 
 public class GameLogic {
-    public static String turn = "O";
+    public static int turn = 0;
+
+
     public static Player[] setPlayers(){
         Player players[] = new Player[2];
         String tempName;
         Scanner in = new Scanner(System.in);
         for(int i = 0; i < 2; i++){
-            System.out.println("Player "+(i+1) + " please enter your name: ");
-            tempName = in.nextLine();
-            players[i] = new Player(tempName);
+            //System.out.println("Player "+(i+1) + " please enter your name: ");
+            //tempName = in.nextLine();
+            players[i] = new Player("player " + i);
         }
         players[0].setPiece("X");
         players[1].setPiece("O");
-        
+        for(int i = 0; i < 2; i++){
+            System.out.println(players[i].getName() + ": " + players[i].getPiece());
+        }
         return players;
     }
 
-    public static String changeTurn() {
-        if(turn == "X"){
-            turn = "O";
+    public static void changeTurn() {
+        if(turn == 0){
+            turn = 1;
         }
         else{
-            turn = "X";
+            turn = 0;
         }
+
+    }
+
+    public static int getTurn(){
         return turn;
     }
 
-    public static Player gameLoop(Board board, Player players[]){
+    public static TilePane gameLoop(Board board, Player players[]){
         int i = 0;
-        Scanner in = new Scanner(System.in);
-        String tempPos;
-        while(!board.checkWin()){
 
+        System.out.print("Hello") ;
+        return board.printBoard();
 
-            i++;
-        }
-        return players[i%2];
     }
 }
